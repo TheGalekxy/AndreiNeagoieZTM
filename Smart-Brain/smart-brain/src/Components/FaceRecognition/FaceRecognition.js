5ac92fc485a635d5850eb7300f2e62642595d98f
Adam Galek <galekxy13@hotmail.com>
2020-03-19 15:29:51 -0400

    smart-brain back end

--- Smart-Brain/smart-brain/src/Components/FaceRecognition/FaceRecognition.js
+++ Smart-Brain/smart-brain/src/Components/FaceRecognition/FaceRecognition.js
@@ -7,7 +7,7 @@ const FaceRecognition = ({ imageUrl, box }) => {
         <div className='center ma'>
             <div className='absolute mt2'>
                 <img id='inputimage' src={imageUrl} alt='face recognition' width='500px' height='auto'/>
-            <div className='surrounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol }}></div>
+            <div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol }}></div>
             {/* Fixed the bug above, instead of being 'bottomRow' it was 'bottowRow' */}
             </div>
         </div>

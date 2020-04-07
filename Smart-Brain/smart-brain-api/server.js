fce6842f2674abe7acb2c44d0fe2b0c6cae7286b
Adam <galekxy13@hotmail.com>
2020-04-07 16:57:42 -0400

    adding server stuff

--- Smart-Brain/smart-brain-api/server.js
+++ Smart-Brain/smart-brain-api/server.js
@@ -2,104 +2,44 @@ const express = require('express');
 // const bcrypt = require('bcrypt-nodejs');
 const bcrypt = require('bcryptjs');
 const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image')
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'postgres',
+      password : 'Meitsme309331239',
+      database : 'smartbrain'
+    }
+  });
+
+
 
 
 const app = express();
 app.use(express.json());
 app.use(cors())
 
-const database = {
-    users: [
-        {
-            id: '123',
-            name: 'John',
-            password: 'cookies',
-            email: 'john@gmail.com',
-            entries: 0,
-            joined: new Date()
-        },
-        {
-            id: '124',
-            name: 'Sally',
-            password: 'bananas',
-            email: 'sally@gmail.com',
-            entries: 0,
-            joined: new Date()
-        }
-    ], 
-    login: [
-        {
-            id: '987',
-            hash: '',
-            email: 'john@gmail.com'
-        }
-    ]
-}
 
 app.get('/', (req, res) =>{
     res.send(database.users);
 })
 
 
-app.post('/signin', (req, res) => {
+app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
 
-        // Load hash from your password DB.
-    bcrypt.compare("apples", "$2a$10$/ereAvN6o5COtuwzc3z8tevaEvYgCcTkfxADXEUTRYuEhFnctxaTy", function(err, res) {
-        console.log('first guess', res)
-        // res == true
-    });
-    bcrypt.compare("veggies", "$2a$10$/ereAvN6o5COtuwzc3z8tevaEvYgCcTkfxADXEUTRYuEhFnctxaTy", function(err, res) {
-        console.log('second guess', res)
-    });
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 
-    if (req.body.email === database.users[0].email && 
-        req.body.password === database.users[0].password) {
-            res.json(database.users[0]);
-        } else {
-            res.status(400).json('error logging in.')
-        }
-})
+app.get('/profile/:id', (req, res) => { msWriteProfilerMark.handleProfileGet(req, res, db)}) // :id syntax means we can enter any id and grab that id through the request.id params
 
-app.post('/register', (req, res) => {
-    const { email, name, password } = req.body // grad the email, name, and password from the request body
-    database.users.push({
-        id: '125',
-        name: name,
-        email: email,
-        entries: 0,
-        joined: new Date()
-    })
-    res.json(database.users[database.users.length-1]);         // Always have to send a response back
-})
-
-app.get('/profile/:id', (req, res) => {
-    const { id } = req.params;
-    let found = false;
-    database.users.forEach(user => {
-        if (user.id === id) {               // if user.id is the same as the user we received from the params
-            found = true;
-            return res.json(user);
-        } 
-    })
-    if (!found) {
-        res.status(400).json('not found');
-    }
-}) // :id syntax means we can enter any id and grab that id through the request.id params
-
-app.put('/image', (req, res) => {
-    const { id } = req.body;        // grabbing the id from the requests body
-    let found = false;
-    database.users.forEach(user => {
-        if (user.id === id) {               // if user.id is the same as the user we received from the params
-            found = true;
-            user.entries++;
-            return res.json(user.entries);
-        } 
-    }) 
-    if (!found) {
-        res.status(400).json('not found');
-    }
-})
+app.put('/image', (req, res) => { image.handleImage(req, res, db)})
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
 
 

0679c91cd8380df2b070927f85d3c96fde5a5d5f
Adam <galekxy13@hotmail.com>
2020-04-07 20:35:28 -0400

    front-end changes

--- Smart-Brain/smart-brain/src/Components/Signin/SignIn.js
+++ Smart-Brain/smart-brain/src/Components/Signin/SignIn.js
@@ -20,7 +20,7 @@ class SignIn extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('http://localhost:3000/signin', {
+        fetch(' https://vast-shore-79028.herokuapp.com/signin', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
